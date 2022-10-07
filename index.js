5c4f97f10cac2f96380729d0f7984f4f3b235c80
Sectly <67114413+Sectly@users.noreply.github.com>
2022-10-07 08:04:34 +0200

    Fixed icons
    Fixed icons

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 const consoleMessageTypes = [
-    "\x1b[30m",
-    "\x1b[31m",
-    "\x1b[32m",
-    "\x1b[33m",
-    "\x1b[34m",
-    "\x1b[35m",
-    "\x1b[36m",
-    "\x1b[37m",
-    "\x1b[38m"
+  "\x1b[30m",
+  "\x1b[31m",
+  "\x1b[32m",
+  "\x1b[33m",
+  "\x1b[34m",
+  "\x1b[35m",
+  "\x1b[36m",
+  "\x1b[37m",
+  "\x1b[38m"
 ];
 
 const consoleMessageIcons = [
-    "×",
-    "√",
-    "i",
-    "?",
-    "⟫",
-    ">",
-    "»",
-    "‼",
-    "›"
+  "×",
+  "√",
+  "‼",
+  "i",
+  "⟫",
+  "»",
+  ">",
+  "?",
+  "›"
 ];
 
 const log = type => {
-    return (...args) => {
-        let outputArg = '';
-        const len = args.length;
-        for (let i in args) {
-            if (i == 0) {
-                outputArg += args[i];
-            } else {
-                outputArg += ' ' + args[i];
-            }
-        }
-        outputArg = consoleMessageTypes[type] + " [" + consoleMessageIcons[type] + "] " + outputArg + consoleMessageTypes[7];
-        console.log(outputArg);
-    };
+  return (...args) => {
+    let outputArg = '';
+    const len = args.length;
+    for (let i in args) {
+      if (i == 0) {
+        outputArg += args[i];
+      } else {
+        outputArg += ' ' + args[i];
+      }
+    }
+    outputArg = consoleMessageTypes[type] + "[" + consoleMessageIcons[(type - 1)] + "] " + outputArg + consoleMessageTypes[7];
+    console.log(outputArg);
+  };
 };
 
 module.exports = {
-    warn: log(3),
-    error: log(1),
-    log: log(7),
-    debug: log(6),
-    success: log(2),
-    stress: log(5),
-    info: log(4),
-    output: log(8)
+  warn: log(3),
+  error: log(1),
+  log: log(7),
+  debug: log(6),
+  success: log(2),
+  stress: log(5),
+  info: log(4),
+  output: log(8)
 };
